b712d96b27de96548943e2a7debc96f5f13fa230
Chris Brown <chris@usemarkup.com>
2017-02-02 18:47:17 +0000

    Add more flexibility in command line arguments

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+const argv = require('yargs').default({
+  copy: true,
+  prefix: ':scrabble-',
+  suffix: ':',
+}).argv;
 const ncp = require('copy-paste');
 
-if (process.argv[2] === undefined) {
-  process.stdout.write('Provide a string to scrabble');
+if (argv._.length === 0) {
+  process.stdout.write('Enter a string to scrabble!');
   process.exit(1);
 }
 
 const str = process.argv[2].toLowerCase();
-const scrabbled = str.split('').filter(char => char.match(/[a-z]/g)).map(letter => `:scrabble-${letter}:`).join(' ');
-process.stdout.write(scrabbled);
+const scrabbled = str
+  .split('')
+  .filter(char => char.match(/[a-z]/g))
+  .map(char => `${argv.prefix}${char}${argv.suffix}`)
+  .join(' ');
+process.stdout.write(`${scrabbled}\n`);
 
-ncp.copy(scrabbled, () => process.stdout.write('Copied to clipboard'));
+if (argv.copy) {
+  ncp.copy(scrabbled, () => process.stdout.write('Copied to clipboard'));
+}

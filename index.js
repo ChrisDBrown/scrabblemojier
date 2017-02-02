1b60edef15e2ad12c2120c6f90fe8e306709d8bc
Chris Brown <chris@usemarkup.com>
2017-02-02 17:59:53 +0000

    Copy the result to the clipboard

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+let ncp = require("copy-paste");
+
 if (process.argv[2] === undefined) {
   console.log("Provide a string to scrabble");
   process.exit(1);
 }
 
 const str = process.argv[2].toLowerCase();
-const scrabbled = str.split("").map(letter => ":scrabble-" + letter + ":").join(" ");
+const scrabbled = str.split("").filter(char => char.match(/[a-z]/g)).map(letter => ":scrabble-" + letter + ":").join(" ");
 console.log(scrabbled);
+
+ncp.copy(scrabbled, () => console.log('Copied to clipboard'));

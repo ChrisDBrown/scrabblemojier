c68e0b037c2ba19de747e348b1b33c45b3118cef
Chris Brown <chris@usemarkup.com>
2017-02-02 22:04:06 +0000

    Permanently change settings with .env file

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const argv = require('yargs').default({
-  copy: true,
-  prefix: ':scrabble-',
-  suffix: ':',
-}).argv;
+const dotenv = require('dotenv');
+// eslint-disable-next-line
+dotenv.load();
+const argv = require('yargs')
+  .env('SCRABBLEMOJIER')
+  .default({
+    copy: true,
+    prefix: ':',
+    suffix: ':',
+    blank: ':blank:',
+  }).argv;
 const ncp = require('copy-paste');
 
-if (argv._.length === 0) {
+if (argv._.length === 0 || typeof argv._[0] !== 'string') {
   process.stdout.write('Enter a string to scrabble!');
   process.exit(1);
 }
@@ -18,7 +24,7 @@ const scrabbled = str
     .filter(char => char.match(/[a-z]/g))
     .map(char => `${argv.prefix}${char}${argv.suffix}`)
     .join(' '))
-  .join('    ');
+  .join(` ${argv.blank} `);
 
 process.stdout.write(`${scrabbled}\n`);
 

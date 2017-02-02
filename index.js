e6eff0723dfe8e3b88ba7564bfc63599a3fc7ef5
Chris Brown <chris@usemarkup.com>
2017-02-02 18:13:09 +0000

    Add AirBnB style guide & apply

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let ncp = require("copy-paste");
+const ncp = require('copy-paste');
 
 if (process.argv[2] === undefined) {
-  console.log("Provide a string to scrabble");
+  process.stdout.write('Provide a string to scrabble');
   process.exit(1);
 }
 
 const str = process.argv[2].toLowerCase();
-const scrabbled = str.split("").filter(char => char.match(/[a-z]/g)).map(letter => ":scrabble-" + letter + ":").join(" ");
-console.log(scrabbled);
+const scrabbled = str.split('').filter(char => char.match(/[a-z]/g)).map(letter => `:scrabble-${letter}:`).join(' ');
+process.stdout.write(scrabbled);
 
-ncp.copy(scrabbled, () => console.log('Copied to clipboard'));
+ncp.copy(scrabbled, () => process.stdout.write('Copied to clipboard'));
